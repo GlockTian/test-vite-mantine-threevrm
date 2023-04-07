62c2e8da1241acc766d122c3edd38733109a678b
GlockTian <petertianzhong@gmail.com>
2023-04-07 16:06:06 +1200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
+import React, { useCallback } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { TextInput } from '@mantine/core';
+import Controls from './Controls';
+import { useToggle, useVRM } from './hooks';
+import Inputs from './Inputs';
+import VRM from './VRM';
 import { ThemeProvider } from './ThemeProvider';
-import { Welcome } from './Welcome/Welcome';
 
-export default function App() {
+const FixedTextInput: React.FC = () => (
+  <div
+    style={{
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: '#fff',
+      padding: '10px',
+      borderTop: '1px solid #ccc',
+    }}
+  >
+    <TextInput placeholder="Type your message here" />
+  </div>
+);
+
+const App: React.FC = () => {
+  const [vrm, loadVRM] = useVRM();
+  const [showGrid, showGridToggle] = useToggle(false);
+
+  const handleFileChange = useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      const url = URL.createObjectURL(event.target.files![0]);
+      await loadVRM(url);
+      URL.revokeObjectURL(url);
+    },
+    [loadVRM]
+  );
+
   return (
     <ThemeProvider>
-      <Welcome />
+      <Inputs onFileChange={handleFileChange} checked={showGrid} onCheckChange={showGridToggle} />
+      <Canvas camera={{ position: [0, 1.5, 2], fov: 60, near: 0.01, far: 1000 }}>
+        <directionalLight />
+        <VRM vrm={vrm} />
+        <Controls />
+        {showGrid && (
+          <>
+            <gridHelper />
+            <axesHelper />
+          </>
+        )}
+      </Canvas>
+      <FixedTextInput />
     </ThemeProvider>
   );
-}
+};
+
+export default App;
---
+++ src/Controls.tsx
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react';
+import { extend, ReactThreeFiber, useFrame, useThree } from 'react-three-fiber';
+import { Vector3 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+extend({ OrbitControls });
+
+// see https://github.com/react-spring/react-three-fiber/issues/11#issuecomment-583599378
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<
+        OrbitControls,
+        // `OrbitControls["new"]` does not work without `typeof`.
+        typeof OrbitControls
+      >;
+    }
+  }
+}
+
+// see https://github.com/react-spring/react-three-fiber/blob/v4.0.20/examples/src/demos/dev/GltfAnimation.js#L39-L44
+const Controls: React.FC = () => {
+  const controls = useRef<OrbitControls>(null);
+  const { camera, gl } = useThree();
+
+  useFrame(() => controls.current?.update());
+
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, gl.domElement]}
+      zoomSpeed={0.5}
+      minPolarAngle={Math.PI / 3}
+      maxPolarAngle={Math.PI / 2.2}
+      minDistance={0.5}
+      maxDistance={0.8}
+      // enableDamping
+      // dampingFactor={0.2}
+      target={[0, 1.2, 0]}
+    />
+  );
+};
+
+export default Controls;
